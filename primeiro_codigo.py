3d54f42a80b93d765da4bdd110dacae8cab76b61
alencar00-ads <alencarne00@gmail.com>
2026-01-20 12:49:13 -0300

    Add files via upload

--- primeiro_codigo.py
+++ primeiro_codigo.py
@@ -1,1 +1,12 @@
 print("Meu primeiro código Python!")
+
+nome = input("Qual seu nome? ")
+print(f"Olá, {nome}! Bem-vindo ao Phyton.")
+
+idade = input("Qual sua idade? ")
+print(f"Você tem {idade} anos. ")
+
+a = int(input("Digite um número: "))
+b = int(input("Digite outro número: "))
+print("A soma é:", a + b)
+
